edef8834904126b13cd7dcb2564869f018150af2
Nelson Amador <rod6214@gmail.com>
2022-04-23 01:28:10 -0300

    Created core

---
+++ PotyCore/IFormatter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotyCore
+{
+    public interface IFormatter
+    {
+        string Encode(byte[] bytes);
+        byte Decode(string bin);
+    }
+}
---
+++ PotyCore/IntelHexFormatter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotyCore
+{
+    public class IntelHexFormatter : IFormatter
+    {
+        public byte Decode(string bin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Encode(byte[] bytes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
