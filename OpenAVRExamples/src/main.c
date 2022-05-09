f974e5ad32002fb73b1f29348ad2e932adb93790
Nelson Amador <rod6214@gmail.com>
2022-05-09 00:25:58 -0300

    Fixes

--- OpenAVRExamples/src/main.c
+++ OpenAVRExamples/src/main.c
@@ -3,4 +3,31 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-void main(){}
+
+void config()
+{
+    PORTB = 0;
+    DDRB = (1 << DD0);
+}
+
+void loop() 
+{
+    if (PORTB & (1 << PB0)) 
+    {
+        PORTB = 0;
+    }
+    else 
+    {
+        PORTB = 1;
+    }
+    _delay_ms(200);
+}
+
+int main() 
+{
+    config();
+    while(1) 
+    {
+        loop();
+    }
+}
