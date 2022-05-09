b1e9ce986f3b212e061b228c4b85266b63260175
Nelson Amador <rod6214@gmail.com>
2022-05-09 02:22:07 -0300

    New definitions

--- AT90ProgrammerCmake/AT90Programmer/src/lib/io_uart.c
+++ AT90ProgrammerCmake/AT90Programmer/src/lib/io_uart.c
@@ -10,24 +10,27 @@
 #include <util/delay.h>
 #include "io_uart.h"
 
-#define UCSRB  _SFR_IO8(0x0A)
-#define UBRRL  _SFR_IO8(0x09)
-#define UCSZ2 2
+// #define UCSRB  _SFR_IO8(0x0A)
+// #define UBRRL  _SFR_IO8(0x09)
+// #define UCSZ2 2
 
 void usart_start()
 {
 	/************************************************************************/
 	/*   UBRR0 = fosc/(16*BAUD) - 1                                         */
 	/************************************************************************/
-	UCSRB = (1 << RXEN) | (1 << TXEN) | (1 << RXCIE) | (1 << TXCIE) | (1 << UCSZ2);
+	// UCSRB = (1 << RXEN) | (1 << TXEN) | (1 << RXCIE) | (1 << TXCIE) | (1 << UCSZ2);
+	UCR = (1 << RXEN) | (1 << TXEN) | (1 << RXCIE) | (1 << TXCIE) | (1 << CHR9);
 	_delay_loop_1(1);
-	UBRRL = 25;
+	// UBRRL = 25;
+	UBRR = 25;
 	_delay_loop_1(1);
 }
 
 void usart_send(char data)
 {
-	UCSRB = UCSRB | (1 << TXB8);
+	// UCSRB = UCSRB | (1 << TXB8);
+	UCR = UCR | (1 << TXB8);
 	UDR = data;
 }
 
