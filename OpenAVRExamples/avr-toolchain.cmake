60003e429dc732204ab2d12d5205db65f5871551
Nelson Amador <rod6214@gmail.com>
2022-05-08 22:03:30 -0300

    Added project without IDE

---
+++ OpenAVRExamples/avr-toolchain.cmake
@@ -0,0 +1,10 @@
+find_program(AVR_CC avr-gcc REQUIRED)
+find_program(AVR_CXX avr-g++ REQUIRED)
+find_program(AVR_OBJCOPY avr-objcopy REQUIRED)
+find_program(AVR_SIZE_TOOL avr-size REQUIRED)
+find_program(AVR_OBJDUMP avr-objdump REQUIRED)
+
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR avr)
+set(CMAKE_C_COMPILER ${AVR_CC})
+set(CMAKE_CXX_COMPILER ${AVR_CXX})
