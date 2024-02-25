c5429c798fac07bfb683dcb2318802b2d36001e7
Nelson Amador <rod6214@gmail.com>
2024-02-25 12:42:24 -0300

    Improvements

--- ExtraPortControllerAtmega328/Controller/src/app/main.c
+++ ExtraPortControllerAtmega328/Controller/src/app/main.c
@@ -15,50 +15,55 @@
 #include <i2cext.h>
 #include <util/twi.h>
 
-static uint8_t data[512];
+#define DOE PINB0
+#define Deactivate_Buffer() (DDRD=0)
+#define Activate_Buffer() (DDRD=255)
 
-void TWI_Receiv_irq(uint8_t value, size_t bytes, int id) 
+void TWI_Receiv_irq(uint8_t value, uint16_t command, size_t bytes, int id) 
 {
-	data[id] = value;
+	if (command == WRITE_EXT_MEMORY) 
+	{
+		Activate_Buffer();
+		PORTD = value;
+	}
+	else if (command == ENABLE_DATA_BUFFER) 
+	{
+		Deactivate_Buffer();
+	}
+	else if (command == DISABLED_DATA_BUFFER) 
+	{
+		Activate_Buffer();
+	}
 }
 
-void TWI_Transmit_irq(volatile uint8_t * pdata, uint8_t command, size_t bytes) 
+void TWI_Transmit_irq(volatile uint8_t * pdata, uint16_t command, size_t bytes) 
 {
 	if (command == READ_EXT_MEMORY) 
 	{
-		DDRD = 0;
+		if (DDRD == 255)
+			DDRD = 0;
 		*pdata = PIND;
 	}
 }
 
 void config()
 {
-	// DDRD = 255;
-	// PORTD = 0;
+	DDRD = 0;
 	MCUCR |= (1 << PUD);
-	// data[0] = 0x33;
-	// data[0] = 2;
-	// data[1] = 2;
-	// data[2] = 2;
-	// data[3] = 2;
-	// data[4] = 2;
-	// EEPROM_init();
 	I2C_Register_Receiving_Callback(TWI_Receiv_irq);
 	I2C_Register_Transmition_Callback(TWI_Transmit_irq);
 	sei();
 	I2C_Slave_Init(I2C_CL_100KHz, 1);
+	// DDRD = 255;
 }
 
 void loop() 
 {
-	// test();
 }
 
 int main(void)
 {
 	config();
-	// write_port(0x999999);
-    // /* Replace with your application code */
     while (1) 
     {
 		loop();
--- MemoryControllerAtmega328/Controller/src/app/main.c
+++ MemoryControllerAtmega328/Controller/src/app/main.c
@@ -15,9 +15,6 @@
 #include <i2cext.h>
 #include <util/twi.h>
 
-void populateROM16bit(void);
-void populateROM();
-
 
 void ERROR() 
 {
@@ -29,109 +26,34 @@ ISR(INT0_vect) {
 	OE_off();
 	R_on();
 	CONTROL_Activate();
-	// populateROM();
-	populateROM16bit();
+	populateROM(L595_BUS_LENGTH_NORMAL);
+	// populateROM16bit(L595_BUS_LENGTH_NORMAL);
 	PORT_Deactivate();
 	CONTROL_Deactivate();
 	HOLD_off();
 	sei();
 }
 
-static uint8_t data[] = {0x22, 0x23, 0x22, 0x23, 0x22, 0x23, 0x22, 0x23, 0x22, 0x23, 0x22, 0x23, 0x22, 0x23, 0x22, 0x23};
-static uint8_t rom_data[16];
+// static uint8_t data[] = {0x22};
+// static uint8_t rom_data[16];
 
 void config()
 {
-	PORTB = 0;
-	PORTD = 0;
-	DDRB = (1 << PORT0);
-	DDRD = 255;
-	_delay_ms(50);
-	MCUCR |= (1 << PUD);
-	
-	Command_t cmd = {
-		3, data, 1, 0xff88, 100
-	};
-
-	I2C_Master_Init(I2C_CL_100KHz);
-
-	if (I2C_Send_Command(cmd)) 
-	{
-		PORTB |= (1 << PORT0);
-	}
-
-	if (I2C_Read_Command(cmd)) 
-	{
-		PORTB |= (1 << PORT0);
-	}
-
-	cmd.command = READ_EXT_MEMORY;
-
-	if (I2C_Read_Command(cmd)) 
-	{
-		PORTB |= (1 << PORT0);
-	}
-
-
-	EEPROM_Read_Page(0,rom_data,16,0);
-
-	// EEPROM_Write(0, 0x36, 0);
-
-	// EEPROM_Write_Page(0, data, 16, 0);
-	
 	// Configuring INT0 as rising edge detection
-	// EICRA |= (1 << ISC01) | (1 << ISC00);
-	// // Enabling INT0 interrupt
-	// EIMSK |= (1 << INT0);
-	// READY_on();
-	// _delay_ms(100);
-	//PORT_Init();
-
-	// DDRD = 255;
-	// PORTD = 0;
-
-	// EEPROM_init(I2C_CL_100KHz);
-
-	// I2C_Master_Start();
-
-	// I2C_Write(1 << 1, I2C_ACK);
-
-	// //I2C_Master_Start();
-
-	// I2C_Write(1, I2C_ACK);
-	// //I2C_Master_Write(0);
-
-	// // _delay_ms(10);
-
-	// I2C_Write(2, I2C_ACK);
-
-	// I2C_Master_Stop();
-
-	// I2C_Master_Start();
-
-	// I2C_Write(1 << 1 | 1, I2C_ACK);
-
-	// PORTD = I2C_Read(I2C_NACK);
-
-	// I2C_Master_Stop();
-	// reset_port();
-	// HOLD_on();
-	// OE_on();
-	// MR_on();
-	// // write_port(0xffffff);
-	// _delay_loop_1(5);
-	
-	
-	// sei();
-
-	// OE_off();
-	// populateROM16bit();
-	// PORT_Deactivate();
-	// HOLD_off();
-	// PORT_Deactivate();
-	// READY_on();
-	// populateROM();
-
+	EICRA |= (1 << ISC01) | (1 << ISC00);
+	// Enabling INT0 interrupt
+	EIMSK |= (1 << INT0);
+	READY_on();
+	_delay_ms(100);
+	PORT_Init();
+	EEPROM_init(I2C_CL_100KHz);
+	reset_port();
+	HOLD_on();
+	OE_on();
+	MR_on();
+	// write_port(0xffffff);
+	_delay_loop_1(5);
+	sei();
 }
 
 
